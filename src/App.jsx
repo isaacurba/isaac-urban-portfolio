5ca4c18b7b2c18a631cb83d491d508e98f36a1a8
isaacurba <kpomassiurban@gmail.com>
2025-12-23 01:59:28 +0100

    updated routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar"
 import Main from "./components/Main"
+import About from "./components/AboutMe"
 function App() {
 
   return (
     <>
         <Navbar />
         <Main />
+        <About />
     </>
   )
 }

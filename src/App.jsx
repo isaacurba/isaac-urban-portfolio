47e2f69a930f5388308faa05b437d06bed3db749
isaacurba <kpomassiurban@gmail.com>
2025-12-23 03:47:17 +0100

    updated app.jsx cos of new components

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import Navbar from "./components/Navbar"
 import Main from "./components/Main"
 import About from "./components/AboutMe"
 import IDo from "./components/WhatIDo"  
+import Projects from "./components/Projects"
+import GetInTouch from "./components/GetInTouch"
 
 function App() {
   return (
     <>
-        <Navbar />
-        <Main />
-        <About />
-        <IDo />  
+      <Navbar />
+      <Main />
+      <About />
+      <IDo />  
+      <Projects />
+      <GetInTouch />
     </>
   )
 }

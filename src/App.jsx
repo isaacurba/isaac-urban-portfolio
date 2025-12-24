c6875c78d0b6f96999c1bfb5e3ad4ab625aa0005
isaacurba <kpomassiurban@gmail.com>
2025-12-24 21:02:08 +0100

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import About from "./components/AboutMe"
 import IDo from "./components/WhatIDo"  
 import Projects from "./components/Projects"
 import GetInTouch from "./components/GetInTouch"
+import Footer from "./components/Footer"
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <IDo />  
       <Projects />
       <GetInTouch />
+      <Footer />
     </>
   )
 }

c01b291cb65b5cc420768ed45fdf286946067816
isaacurba <kpomassiurban@gmail.com>
2025-12-25 18:40:41 +0100

    fixed bug in omport

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Navbar from "./components/Navbar"
 import Main from "./components/Main"
 import About from "./components/AboutMe"
 import IDo from "./components/WhatIDo"  
-import Projects from "./components/Projects"
+import Projects from "./components/Projects.jsx"
 import GetInTouch from "./components/GetInTouch"
 import Footer from "./components/Footer"
 

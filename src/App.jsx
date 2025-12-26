de07ca4e7ca645725d120077daca6c10080a7ef3
isaacurba <kpomassiurban@gmail.com>
2025-12-27 00:23:28 +0100

    Fixed the app.jsx to instaed of having individual component in the app.jsx i just added it to itsown separate component them imported it to app.jx to avoid router conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { Routes, Route } from "react-router-dom"
 import AllProjects from "./pages/AllProjects.jsx"
 import Home from "./pages/Home.jsx"
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+
 function App() {
   return (
     <>
-
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/projects" element={<AllProjects/>}/>
       </Routes>
-
+      <Footer />
     </>
   )
 }

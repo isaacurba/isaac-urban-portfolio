094c61855e9d445f22bcec6f67e9cf1dc2515372
isaacurba <kpomassiurban@gmail.com>
2025-12-28 08:47:25 +0100

    Created separate files for all Projects

---
+++ src/pages/allProjects.js
@@ -0,0 +1,53 @@
+import MovieApp from "../assets/images/movieApp.png"
+
+export const allProjects = [
+    {
+        image: MovieApp,
+        name: "Movie App",
+        description: "Lorem20kdhcydegcudhgcvdbccougvyi8dcvdhvbiuegvedbvdegfceydfckjbIGYCHIDVICGDVBDIYGDICVBDIYVGEIHBJNPIOCV",
+        techs: ["html", "css", "JavaScript", "react", "tailwindCss", "react"],
+        url: "https://movie-app-one-henna-65.vercel.app/",
+        category:"Frontend"
+    },
+    {
+        image: MovieApp,
+        name: "Movie App",
+        description: "Lorem20kdhcydegcudhgcvdbccougvyi8dcvdhvbiuegvedbvdegfceydfckjbIGYCHIDVICGDVBDIYGDICVBDIYVGEIHBJNPIOCV",
+        techs: ["html", "css", "JavaScript", "react", "tailwindCss", "react"],
+        url: "https://movie-app-one-henna-65.vercel.app/",
+        category:"Backend"        
+    },
+    {
+        image: MovieApp,
+        name: "Movie App",
+        description: "Lorem20kdhcydegcudhgcvdbccougvyi8dcvdhvbiuegvedbvdegfceydfckjbIGYCHIDVICGDVBDIYGDICVBDIYVGEIHBJNPIOCV",
+        techs: ["html", "css", "JavaScript", "react", "tailwindCss", "react"],
+        url: "https://movie-app-one-henna-65.vercel.app/",
+        category:"Fullstack"
+    },
+    {
+        image: MovieApp,
+        name: "Movie App",
+        description: "Lorem20kdhcydegcudhgcvdbccougvyi8dcvdhvbiuegvedbvdegfceydfckjbIGYCHIDVICGDVBDIYGDICVBDIYVGEIHBJNPIOCV",
+        techs: ["html", "css", "JavaScript", "react", "tailwindCss", "react"],
+        url: "https://movie-app-one-henna-65.vercel.app/",
+        category:"API"
+    },
+    {
+        image: MovieApp,
+        name: "Movie App",
+        description: "Lorem20kdhcydegcudhgcvdbccougvyi8dcvdhvbiuegvedbvdegfceydfckjbIGYCHIDVICGDVBDIYGDICVBDIYVGEIHBJNPIOCV",
+        techs: ["html", "css", "JavaScript", "react", "tailwindCss", "react"],
+        url: "https://movie-app-one-henna-65.vercel.app/",
+        category:"Database"
+    },
+    {
+        image: MovieApp,
+        name: "Movie App",
+        description: "Lorem20kdhcydegcudhgcvdbccougvyi8dcvdhvbiuegvedbvdegfceydfckjbIGYCHIDVICGDVBDIYGDICVBDIYVGEIHBJNPIOCV",
+        techs: ["html", "css", "JavaScript", "react", "tailwindCss", "react"],
+        url: "https://movie-app-one-henna-65.vercel.app/",
+        category:"Frontend"
+    },                    
+
+]

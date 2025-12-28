bb96de80a71aa176a0497696832e8da8398236f5
isaacurba <kpomassiurban@gmail.com>
2025-12-28 23:25:38 +0100

    Removedd the nav adn footer from home component instead added to app.jsx file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,16 @@
-import Navbar from "../components/Navbar"
 import Main from "../components/Main"
 import About from "../components/AboutMe"
 import IDo from "../components/WhatIDo"  
 import Projects from "../components/Project.jsx"
 import GetInTouch from "../components/GetInTouch"
-import Footer from "../components/Footer"
 const Home = () => {
   return (
     <div>
-        <Navbar />
         <Main />
         <About />
         <IDo />  
         <Projects />
         <GetInTouch />
-        <Footer />
     </div>
   )
 }

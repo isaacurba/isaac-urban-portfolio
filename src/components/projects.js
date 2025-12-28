7557ec88c9de53b4290e8267953b79e93059565d
isaacurba <kpomassiurban@gmail.com>
2025-12-28 23:23:37 +0100

    Put right project details

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,45 +1,49 @@
-import MovieApp from "../assets/images/movieApp.png"
+import MovieApp from "../assets/images/movieApp.png";
+import LightCode from "../assets/images/lightCode.png";
+import CrispyKitchen from "../assets/images/krispyScreenshot.png"
+import NewsLetterSignUp from "../assets/images/newsletter.png"
+import MagloDashboard from "../assets/images/magloDashboard.png"
 
 export const projects = [
     {
         image: MovieApp,
         name: "Movie App",
-        description: "Lorem20kdhcydegcudhgcvdbccougvyi8dcvdhvbiuegvedbvdegfceydfckjbIGYCHIDVICGDVBDIYGDICVBDIYVGEIHBJNPIOCV",
-        techs: ["html", "css", "JavaScript", "react", "tailwindCss", "react"],
+        description: "MovieApp – A Netflix-style React app to discover movies, search titles, and save favourites using the TMDB API",
+        techs: ["react", "tailwindCss", "TMDB-API"],
         url: "https://movie-app-one-henna-65.vercel.app/",
-        category:"Frontend"
+        category:["Frontend", "API"]
     },
     {
-        image: MovieApp,
-        name: "Movie App",
-        description: "Lorem20kdhcydegcudhgcvdbccougvyi8dcvdhvbiuegvedbvdegfceydfckjbIGYCHIDVICGDVBDIYGDICVBDIYVGEIHBJNPIOCV",
-        techs: ["html", "css", "JavaScript", "react", "tailwindCss", "react"],
-        url: "https://movie-app-one-henna-65.vercel.app/",
-        category:"Backend"        
+        image: LightCode,
+        name: "Light Code",
+        description: "A pixel-perfect implementation of a modern Figma design, brought to life with clean and responsive front-end code. The project showcases attention to design details and smooth layout transitions.",
+        techs: ["react", "tailwindCss"],
+        url: "https://light-code-one.vercel.app/",
+        category:"Frontend"        
     },
     {
-        image: MovieApp,
+        image: CrispyKitchen,
         name: "Movie App",
-        description: "Lorem20kdhcydegcudhgcvdbccougvyi8dcvdhvbiuegvedbvdegfceydfckjbIGYCHIDVICGDVBDIYGDICVBDIYVGEIHBJNPIOCV",
-        techs: ["html", "css", "JavaScript", "react", "tailwindCss", "react"],
+        description: "A full stack restaurant reservation website",
+        techs: ["Node.js", "express", "EJS"],
         url: "https://movie-app-one-henna-65.vercel.app/",
         category:"Fullstack"
     },
     {
-        image: MovieApp,
-        name: "Movie App",
-        description: "Lorem20kdhcydegcudhgcvdbccougvyi8dcvdhvbiuegvedbvdegfceydfckjbIGYCHIDVICGDVBDIYGDICVBDIYVGEIHBJNPIOCV",
-        techs: ["html", "css", "JavaScript", "react", "tailwindCss", "react"],
-        url: "https://movie-app-one-henna-65.vercel.app/",
-        category:"API"
+        image: NewsLetterSignUp,
+        name: "NewsLetter Signup",
+        description: " Newsletter Signup App A simple newsletter signup app",
+        techs: ["html", "css", "JavaScript", "Node.js", "express"],
+        url: "https://newsletter-signup-mwbu.onrender.com/",
+        category:["Fullstack", "Backend", "API"]
     },
     {
-        image: MovieApp,
-        name: "Movie App",
-        description: "Lorem20kdhcydegcudhgcvdbccougvyi8dcvdhvbiuegvedbvdegfceydfckjbIGYCHIDVICGDVBDIYGDICVBDIYVGEIHBJNPIOCV",
-        techs: ["html", "css", "JavaScript", "react", "tailwindCss", "react"],
+        image: MagloDashboard,
+        name: "Maglo Finance Dashboard",
+        description: "A modern, responsive invoicing application built with React that allows users to create, manage, and track invoices.",
+        techs: ["react", "tailwindCss", "Schadcn Ui", "AppWrite"],
         url: "https://movie-app-one-henna-65.vercel.app/",
-        category:"Database"
+        category:["Database", "Frontend", "Backend", "API"]
     },
     {
         image: MovieApp,
